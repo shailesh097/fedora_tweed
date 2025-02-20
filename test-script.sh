4d288988e489d10263106188a796799501515b99
shailesh097 <sailesh.pokharel.234@gmail.com>
2025-02-21 00:04:13 +0100

    commit before branching

--- test-script.sh
+++ test-script.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
 
-BOLD="$(tput bold 2>/dev/null || printf '')"
-GREY="$(tput setaf 7 2>/dev/null || printf '')"
-RED="$(tput setaf 1 2>/dev/null || printf '')"
-RED="$(tput setaf 1 2>/dev/null || printf '')"
-GREEN="$(tput setaf 2 2>/dev/null || printf '')"
-YELLOW="$(tput setaf 3 2>/dev/null || printf '')"
-BLUE="$(tput setaf 4 2>/dev/null || printf '')"
-MAGENTA="$(tput setaf 5 2>/dev/null || printf '')"
-NO_COLOR="$(tput sgr0 2>/dev/null || printf '')"
-LIGHT_GREEN="$(tput setaf 10 2>/dev/null || printf '')"
-CYAN="$(tput setaf 37 2>/dev/null || printf '')"
+clipboard-indicator(){
+  git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
 
-
-info() {
-  printf '%s\n' "${CYAN}>>> $* ${NO_COLOR} "
 }
 
-warn() {
-  printf '%s\n' "${YELLOW}! $*${NO_COLOR}"
-}
-
-error() {
-  printf '%s\n' "${RED}x $*${NO_COLOR}" >&2
-}
-
-completed() {
-  printf '%s\n' "${GREEN}✓ $* ${NO_COLOR} "
-}
-
-mylist=("value2" "value3")
-append="value4"
-
-newlist=${mylist[@]/), $append/)}
-
-info ${newlist[@]}
-
+clipboard-indicator
