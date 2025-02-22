af3be24830ac6b71c949a94c1d02cc5c381cc96c
shailesh097 <sailesh.pokharel.234@gmail.com>
2025-02-23 00:38:10 +0100

    added src folder

--- test-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clipboard-indicator(){
-  git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
-}
-
-clipboard-indicator
