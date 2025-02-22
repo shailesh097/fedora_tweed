f82cfa263c475bcbb9703690f1004c37e80fb7c0
shailesh097 <sailesh.pokharel.234@gmail.com>
2025-02-22 16:59:59 +0100

    Added Gnome Shell Extension: clipboard-indicator

--- test-script.sh
+++ test-script.sh
@@ -2,7 +2,6 @@
 
 clipboard-indicator(){
   git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
-
 }
 
 clipboard-indicator
