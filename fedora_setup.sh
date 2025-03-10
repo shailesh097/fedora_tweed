05c4eda3f210ae3b2b27d6d176d8b5ef1b83e0dd
shailesh097 <sailesh.pokharel.234@gmail.com>
2025-03-10 01:09:01 +0100

    fixed bugs in syncing tool setup

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -35,7 +35,7 @@ setup_fedora(){
 
   #install eDM Mononstall curl wget git neovim fzf conky kitty fish nvtop btop
   info "Installing Essential Tools..."
-  sudo dnf install -y curl wget git neovim fzf conky kitty fish nvtop btop fastfetch npm eza gnome-tweaks gnome-shell-extensions discord vlc gparted bash
+  sudo dnf install -y curl wget git neovim fzf conky kitty fish nvtop btop fastfetch npm eza gnome-tweaks gnome-shell-extensions discord vlc gparted bash shc
   sleep 2
 
   # Set Catppuccin-Mocha theme for kitty
@@ -120,14 +120,14 @@ setup_fedora(){
 # install dotfiles sync
 install_dotfiles_sync(){
   info "Installing dotfiles syncing script..."
-  
-  local BIN_SCRIPT_DIR="$HOME/git_clone/fedora_tweed/bins/dotfiles_sync.sh"
+  local BIN_SCRIPT="$HOME/git_clone/fedora_tweed/bins/dotfiles_sync.sh"
+  local BIN_FILE="$HOME/git_clone/fedora_tweed/bins/sync-dotfiles"
   local BIN_DIR="$HOME/.local/bin"
-
   mkdir -p $BIN_DIR
-  chmod +x "$BIN_SCRIPT_DIR" 
-
-  cp "$BIN_SCRIPT_DIR" "$BIN_DIR"
+  chmod +x "$BIN_SCRIPT"
+  # compile $BIN_SCRIPT
+  shc -f $BIN_SCRIPT -o "$BIN_FILE"
+  cp "$BIN_FILE" "$BIN_DIR"
 }
 
 # change wallpaper every 5 minutes
--- test_script.sh
+++ test_script.sh
@@ -2,20 +2,21 @@
 
 source color_setup.sh
 
+
 # install dotfiles sync
 install_dotfiles_sync(){
   info "Installing dotfiles syncing script..."
   
-  local BIN_SCRIPT_DIR="$HOME/git_clone/fedora_tweed/bins/dotfiles_sync.sh"
+  local BIN_SCRIPT="$HOME/git_clone/fedora_tweed/bins/dotfiles_sync.sh"
+  local BIN_FILE="$HOME/git_clone/fedora_tweed/bins/sync-dotfiles"
   local BIN_DIR="$HOME/.local/bin"
 
-  # set user path for fish shell
-  set -U fish_user_paths $fish_user_paths ~/.local/bin
-
   mkdir -p $BIN_DIR
-  chmod +x "$BIN_SCRIPT_DIR" 
+  chmod +x "$BIN_SCRIPT"
 
-  cp "$BIN_SCRIPT_DIR" "$BIN_DIR"
+  # compile $BIN_SCRIPT
+  shc -f $BIN_SCRIPT -o "$BIN_FILE"
+  cp "$BIN_FILE" "$BIN_DIR"
 }
 
 install_dotfiles_sync
