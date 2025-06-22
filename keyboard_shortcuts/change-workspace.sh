70e11198f2795ecb8974a9373f656f589ae4a1e2
shailesh097 <sailesh.pokharel.234@gmail.com>
2025-06-22 13:03:40 +0200

    changed changing workspace shortcut to <super>h instead of <super>j

--- keyboard_shortcuts/change-workspace.sh
+++ keyboard_shortcuts/change-workspace.sh
@@ -8,19 +8,19 @@ remove_lock_keybind(){
 
 change_workspace(){
   # Set Super+j to switch to the workspace on the left
-  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>j']"
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>h']"
 
   # Set Super+l to switch to the workspace on the right
   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>l']"
 
   # Set new keybindings to move the current window to the workspace to the left and right
-  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Shift><Super>j']"
+  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Shift><Super>h']"
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Shift><Super>l']"
 
   info "Custom shortcuts set:"
-  info "  - Super+j to switch to previous workspace"
+  info "  - Super+h to switch to previous workspace"
   info "  - Super+l to switch to next workspace"
-  info "  - Shift+Super+j to move current window 1 workspace to the left"
+  info "  - Shift+Super+h to move current window 1 workspace to the left"
   info "  - Shift+Super+l to move current window 1 workspace to the right"
 }
 
